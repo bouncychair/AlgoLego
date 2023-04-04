633aedb4ddd0a03cdc438f46203e57493c213506
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-04-04 15:33:30 +0200

    Made UI and added the insert CSV

--- C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.QuickSort_radio = new System.Windows.Forms.RadioButton();
+            this.BubbleSort_radio = new System.Windows.Forms.RadioButton();
+            this.LinearSearch_radio = new System.Windows.Forms.RadioButton();
+            this.BinarySearch_radio = new System.Windows.Forms.RadioButton();
+            this.Search_bttn = new System.Windows.Forms.Button();
+            this.insert_bttn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.legoSets = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.legoSets)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // QuickSort_radio
+            // 
+            this.QuickSort_radio.AutoSize = true;
+            this.QuickSort_radio.Location = new System.Drawing.Point(635, 182);
+            this.QuickSort_radio.Name = "QuickSort_radio";
+            this.QuickSort_radio.Size = new System.Drawing.Size(86, 20);
+            this.QuickSort_radio.TabIndex = 0;
+            this.QuickSort_radio.TabStop = true;
+            this.QuickSort_radio.Text = "QuickSort";
+            this.QuickSort_radio.UseVisualStyleBackColor = true;
+            // 
+            // BubbleSort_radio
+            // 
+            this.BubbleSort_radio.AutoSize = true;
+            this.BubbleSort_radio.Location = new System.Drawing.Point(635, 226);
+            this.BubbleSort_radio.Name = "BubbleSort_radio";
+            this.BubbleSort_radio.Size = new System.Drawing.Size(95, 20);
+            this.BubbleSort_radio.TabIndex = 1;
+            this.BubbleSort_radio.TabStop = true;
+            this.BubbleSort_radio.Text = "BubbleSort";
+            this.BubbleSort_radio.UseVisualStyleBackColor = true;
+            // 
+            // LinearSearch_radio
+            // 
+            this.LinearSearch_radio.AutoSize = true;
+            this.LinearSearch_radio.Location = new System.Drawing.Point(635, 274);
+            this.LinearSearch_radio.Name = "LinearSearch_radio";
+            this.LinearSearch_radio.Size = new System.Drawing.Size(108, 20);
+            this.LinearSearch_radio.TabIndex = 2;
+            this.LinearSearch_radio.TabStop = true;
+            this.LinearSearch_radio.Text = "LinearSearch";
+            this.LinearSearch_radio.UseVisualStyleBackColor = true;
+            // 
+            // BinarySearch_radio
+            // 
+            this.BinarySearch_radio.AutoSize = true;
+            this.BinarySearch_radio.Location = new System.Drawing.Point(635, 322);
+            this.BinarySearch_radio.Name = "BinarySearch_radio";
+            this.BinarySearch_radio.Size = new System.Drawing.Size(109, 20);
+            this.BinarySearch_radio.TabIndex = 3;
+            this.BinarySearch_radio.TabStop = true;
+            this.BinarySearch_radio.Text = "BinarySearch";
+            this.BinarySearch_radio.UseVisualStyleBackColor = true;
+            // 
+            // Search_bttn
+            // 
+            this.Search_bttn.Location = new System.Drawing.Point(635, 421);
+            this.Search_bttn.Name = "Search_bttn";
+            this.Search_bttn.Size = new System.Drawing.Size(116, 46);
+            this.Search_bttn.TabIndex = 4;
+            this.Search_bttn.Text = "Search";
+            this.Search_bttn.UseVisualStyleBackColor = true;
+            // 
+            // insert_bttn
+            // 
+            this.insert_bttn.Location = new System.Drawing.Point(60, 515);
+            this.insert_bttn.Name = "insert_bttn";
+            this.insert_bttn.Size = new System.Drawing.Size(75, 23);
+            this.insert_bttn.TabIndex = 6;
+            this.insert_bttn.Text = "Insert";
+            this.insert_bttn.UseVisualStyleBackColor = true;
+            this.insert_bttn.Click += new System.EventHandler(this.insert_bttn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(55, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "LEGO ALGORITHMS";
+            // 
+            // legoSets
+            // 
+            this.legoSets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.legoSets.Location = new System.Drawing.Point(60, 111);
+            this.legoSets.Name = "legoSets";
+            this.legoSets.RowHeadersWidth = 51;
+            this.legoSets.RowTemplate.Height = 24;
+            this.legoSets.Size = new System.Drawing.Size(548, 379);
+            this.legoSets.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(972, 568);
+            this.Controls.Add(this.legoSets);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.insert_bttn);
+            this.Controls.Add(this.Search_bttn);
+            this.Controls.Add(this.BinarySearch_radio);
+            this.Controls.Add(this.LinearSearch_radio);
+            this.Controls.Add(this.BubbleSort_radio);
+            this.Controls.Add(this.QuickSort_radio);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.legoSets)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton QuickSort_radio;
+        private System.Windows.Forms.RadioButton BubbleSort_radio;
+        private System.Windows.Forms.RadioButton LinearSearch_radio;
+        private System.Windows.Forms.RadioButton BinarySearch_radio;
+        private System.Windows.Forms.Button Search_bttn;
+        private System.Windows.Forms.Button insert_bttn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView legoSets;
     }
 }
 
--- C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,59 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void insert_bttn_Click(object sender, EventArgs e)
+        {
+            //open the filedialog for searching for .csv file
+            FileDialog fd = new OpenFileDialog()
+            {
+                Filter = "CSV Files (*.csv)|*.csv",
+            };
+            if (fd.ShowDialog().Equals(DialogResult.OK))
+            {
+                DataTable dt = new DataTable();
+                try
+                {
+                    using (StreamReader sr = new StreamReader(fd.FileName))
+                    {
+
+                        // Will tell if it is the first row
+                        bool firstRow = true;
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] lineElements = line.Split(',');
+
+                            if (firstRow)
+                            {
+                                // The first row defines the column names
+                                foreach (string title in lineElements)
+                                {
+                                    dt.Columns.Add(title);
+                                }
+
+                                firstRow = false;
+                            }
+                            else
+                            {
+                                // Adding values to the DataTable
+                                dt.Rows.Add(lineElements);
+                            }
+                        }
+                    }
+                    legoSets.DataSource = dt;
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a valid file!");
+            }
+
         }
     }
 }
