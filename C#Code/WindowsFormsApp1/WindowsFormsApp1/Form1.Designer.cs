7fbe07049d4e11149ddaeffc4b60eb5a794d4a17
Corvin Wittmaack <corvin.wittmaack@student.nhlstenden.com>
2023-04-06 14:16:54 +0200

    made the ui better and added dropdown

--- C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ C#Code/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -127,7 +127,8 @@
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Items.AddRange(new object[] {
             "ArrayList",
-            "LinkedList"});
+            "LinkedList",
+            "BinaryTree"});
             this.comboBox1.Location = new System.Drawing.Point(635, 99);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(203, 24);
